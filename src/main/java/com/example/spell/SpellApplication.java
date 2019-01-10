69224f49e86f00d90583fafd69d423139fba6830
jetme1 <gomez_andrew@hotmail.com>
2019-01-10 11:14:32 -0500

    my first commit

---
+++ src/main/java/com/example/spell/SpellApplication.java
@@ -0,0 +1,14 @@
+package com.example.spell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpellApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpellApplication.class, args);
+    }
+
+}
+
